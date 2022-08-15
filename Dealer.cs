2b82f8f92be10d84fdcd4bc0d0fae7081dc5e42f
PausedMagician <Iamtriggerhapy@gmail.com>
2022-08-15 12:48:32 +0200

    Intial
    Co-authored-by: Flo908 <Flo908@users.noreply.github.com>
    Co-authored-by: ActuallyJustAVirus <ActuallyJustAVirus@users.noreply.github.com>

---
+++ Dealer.cs
@@ -0,0 +1,12 @@
+class Dealer 
+{
+    public Car_Class[] Cars = {new Car_Class("F"),};
+    public bool buycar() 
+    {
+        return false;
+    }
+    public bool sellcar() 
+    {
+        return false;
+    }
+}
