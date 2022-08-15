1844e4d12a3cfdaf33469f25afa683b6b2951f61
PausedMagician <iamtriggerhapy@gmail.com>
2022-08-15 14:55:10 +0200

    Auto stash before merge of "main" and "origin/main"

--- Dealer.cs
+++ Dealer.cs
@@ -1,5 +1,12 @@
 class Dealer 
 {
+
+    private string Type;
+
+    void Dealer(string Type){
+        this._Type = Type;
+    }
+
     public Car_Class[] Cars = {new Car_Class("F"),};
     public bool buycar() 
     {
---
+++ Person.cs
@@ -0,0 +1,7 @@
+class Person
+{
+    private Car[] Cars_Owned = [];
+    
+    private int Money = 0;
+    
+}
